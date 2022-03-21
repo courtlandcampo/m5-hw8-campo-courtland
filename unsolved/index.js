7870a5dbbe07d3f43a651056a8889d1e7e4bb531
courtlandcampo <courtland.campo@ufl.edu>
2022-03-20 23:01:52 -0400

    Error(s) fixed;

--- unsolved/index.js
+++ unsolved/index.js
@@ -1,5 +1,5 @@
 // Create an Array of at least 3 losing messages
-let losingMessage = ["That ain't it! But you should try again.", "Nope, not that one.", "Try again"];
+let losingMessage = ["Bummer, you lost!", "You Lose.", "Try again, because you lost!"];
 console.log(losingMessage);
 
 // Create variables to count wins and losses
@@ -26,15 +26,14 @@ for (var i = 0; i < box.length; i++) {
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
 function clickedBox(event) {
-    clickedBox.textContent = event.target.textContent;
-    var numberOfBoxClicked = Number(event.target.textContent);
-    console.log(event.target)
-}
+   clickedBox.textContent = event.target.textContent;
+   var numberOfBoxClicked = Number(event.target.textContent);
+   console.log(event.target)
 
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
-var randomNumberWinner = Math.floor(Math.random() * 3) + 1;
-console.log(randomNumberWinner);
+  // create a random number between 1-3 and store it to a variable
+  // This number will represent the winning box
+  var randomNumberWinner = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumberWinner);
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
@@ -42,17 +41,18 @@ console.log(randomNumberWinner);
 
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
+
 if (numberOfBoxClicked === randomNumberWinner) {
-    console.log("You won!");
-    message.textContent = "You won!";
-    winScore += 1;
-    wins.textContent = "Wins:" + winScore;
-    console.log("Your win score:", winScore);
-    } else {
-      console.log(losingMessage);
-    message.textContent = losingMessage[Math.floor(Math.random() * 3)];
-    loseScore += 1;
-    losses.textContent = "Losses:" + loseScore;
-    console.log("Your lose score:", loseScore);
-    }
+  console.log("You won!");
+  message.textContent = "You won!";
+  winScore += 1;
+  wins.textContent = "Wins:" + winScore;
+  console.log("Your win score:", winScore);
+  } else {
+    console.log(losingMessage);
+  message.textContent = losingMessage[Math.floor(Math.random() * 3)];
+  loseScore += 1;
+  losses.textContent = "Losses:" + loseScore;
+  console.log("Your lose score:", loseScore);
+  }
 }
